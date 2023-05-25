23ae4cc6c1fe262aee4f4ac671bfb8827a7b5c93
Danii Saahir <131199603+daniisaahir@users.noreply.github.com>
2023-05-25 23:23:38 +0800

    Smaller code

--- dinosub.py
+++ dinosub.py
@@ -1,67 +1,32 @@
 import os
 import requests
-from concurrent.futures import ThreadPoolExecutor
-import platform
-from pyfiglet import Figlet
+from concurrent.futures import ThreadPoolExecutor as Pool
 
 def install_required_modules():
-    modules = ["requests", "pyfiglet", "setuptools"]
-
-    for module in modules:
-        try:
-            __import__(module)
-        except ImportError:
-            os.system(f"pip install {module}")
+    [__import__(m) for m in ["requests"] if not __import__(m, globals(), locals(), [], 0)]
 
 def clear_terminal():
-    if platform.system() == "Windows":
-        os.system("cls")
-    else:
-        os.system("clear")
+    os.system("cls" if os.name == "nt" else "clear")
 
 def get_subdomains(domain):
-    url = f"https://crt.sh/?q=%.{domain}&output=json"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        subdomains = list(set(entry['name_value'] for entry in data))
-        return subdomains
-
-    return []
+    response = requests.get(f"https://crt.sh/?q=%.{domain}&output=json")
+    return {entry['name_value'] for entry in response.json()} if response.status_code == 200 else set()
 
 def save_subdomains_to_file(subdomains, filename):
     folder_path = os.path.join(os.path.dirname(__file__), "subdomains")
     os.makedirs(folder_path, exist_ok=True)
-    file_path = os.path.join(folder_path, filename)
-
-    with open(file_path, 'w') as file:
-        for subdomain in subdomains:
-            file.write(subdomain + '\n')
+    with open(os.path.join(folder_path, filename), 'w') as f:
+        f.write('\n'.join(subdomains))
 
 def main():
     install_required_modules()
     clear_terminal()
-
-    # Figlet Banner
-    banner = Figlet(font='slant')
-    print(banner.renderText("DinoSub"))
-    print("Author: https://github.com/daniisaahir\n")
-
+    print("DinoSub\nAuthor: https://github.com/daniisaahir\n")
     domain = input("Target Domain: ")
-    print(f"Scanning subdomains for {domain}...")
-
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(get_subdomains, domain)
-        subdomains = future.result()
-
-    print(f"Subdomains found: {len(subdomains)}")
-    print("------------------------------------")
-    for subdomain in subdomains:
-        print(subdomain)
-
-    save_option = input("Save results to txt file? (y/n): ")
-    if save_option.lower() == 'y':
+    subdomains = Pool().submit(get_subdomains, domain).result()
+    print(f"Subdomains found: {len(subdomains)}\n{'-' * 36}")
+    print(*subdomains, sep='\n')
+    if input("Save results to txt file? (y/n): ").lower() == 'y':
         filename = input("Filename: ")
         if not filename.endswith(".txt"):
             filename += ".txt"
